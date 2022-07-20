68152908cf72679ae3cb7ed3fb299cf6f289add1
huangyangcong <huangyangcong@gmail.com>
2022-07-20 09:43:44 +0800

    Modify the code returned as success 200

--- app/controller/base_controller.js
+++ app/controller/base_controller.js
@@ -6,7 +6,7 @@ class BaseController extends Controller {
   success(data, dataKey = 'data', message) {
     message = message || 'success';
     this.ctx.body = {
-      code: 0,
+      code: 200,
       message,
       [dataKey]: data,
     };
